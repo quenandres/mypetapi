404ef80f02dd60170d28853784a5ce1508c0ca6b
Jhonatan Mejia <quenandres@gmail.com>
2019-11-03 19:45:19 -0500

    Inicializa los modelos y los controladores otra vez.

---
+++ database/migrations/2019_11_04_003759_create_compra_productos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompraProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compra_producto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compra_producto');
+    }
+}
